7dd7a1df642af27c1439105f558e26139237f23d
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-02-14 20:40:35 -0600

    fixed

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,74 @@
+#!/usr/bin/python3
+"""Defines all common attributes/methods for other classes
+"""
+import uuid
 from datetime import datetime
-from uuid import uuid4
 import models
 
+
 class BaseModel:
+    """Base class for all models"""
+
     def __init__(self, *args, **kwargs):
+        """Initialization of a Base instance.
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
         if kwargs:
-            self.id = kwargs.get('id', str(uuid4()))
-            created_at = kwargs.get('created_at')
-            self.created_at = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%S.%f") if created_at else datetime.now()
-            updated_at = kwargs.get('updated_at')
-            self.updated_at = datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%S.%f") if updated_at else datetime.now()
+            dtime_format = '%Y-%m-%dT%H:%M:%S.%f'
             for key, value in kwargs.items():
-                if key not in ['id', 'created_at', 'updated_at', '__class__']:
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(
+                        kwargs['created_at'], dtime_format)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        kwargs['updated_at'], dtime_format)
+                else:
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
+            self.updated_at = datetime.now()
+            models.storage.new(self)  # as instructed also in task 5
 
     def __str__(self):
-        return f'[{type(self).__name__}] ({self.id}) {self.__dict__}'
+        """Returns a readable string representation
+        of BaseModel instances"""
+
+        clsName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
 
     def save(self):
+        """Updates the public instance attribute updated_at
+        with the current datetime"""
+
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        change_dict = self.__dict__.copy()
-        change_dict['__class__'] = self.__class__.__name__
-        change_dict['created_at'] = self.created_at.isoformat()
-        change_dict['updated_at'] = self.updated_at.isoformat()
-        return change_dict
+        """Returns a dictionary that contains all
+        keys/values of the instance"""
+        my_dict = self.__dict__.copy()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['__class__'] = self.__class__.__name__
+        # OR
+        # my_dict = dict()
+        # my_dict['__class__'] = self.__class__.__name__
+        # for key, value in self.__dict__.items():
+        #    if key in ('created_at', 'updated_at'):
+        #        my_dict[key] = value.isoformat()
+        #    else:
+        #        my_dict[key] = value
+        return my_dict
+        # my_dict = dict()
+        # my_dict['__class__'] = self.__class__.__name__
+        # for key, value in self.__dict__.items():
+        #    if type(value) is datetime:
+        #        my_dict[key] = value.isoformat()
+        #    else:
+        #        my_dict[key] = value
+        # return my_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,33 +1,74 @@
-""" class FileStorage
-serializes instances to a JSON file
-and deserializes JSON file to instances """
+#!/usr/bin/python3
+"""
+Serializes instances to a JSON file and
+deserializes JSON file to instances.
+"""
+
 import json
 import os
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+class_dict = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "Place": Place,
+    "Amenity": Amenity,
+    "City": City,
+    "Review": Review,
+    "State": State
+}
+# Filestorage == type(self)
+
 
 class FileStorage:
-""" construct """
-__file_path = "file.json"
-__objects = {}
+    """The file storage engine class, that is;
+    A class that serialize and deserialize instances to a JSON file
+    """
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-    """ returns a dictionary of all objects """
-        return FileStorage.__objects
+        """Returns the dictionary of objects"""
+        return type(self).__objects
 
     def new(self, obj):
-    """ adds a new object to the dictionary with the key being the class name and id of the object """
-        FileStorage.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
+        """Sets new obj in __objects dictionary."""
+        if obj.id in type(self).__objects:
+            print("exists")
+            return
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        type(self).__objects[key] = obj
+        # OR
+        # type(self).__objects[obj.id] = obj
 
     def save(self):
-    """ serializes the objects in the dictionary and saves it to a JSON file """
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as fname:
-            new_dict = {key: obj.to_dict() for key, obj in
-                    FileStorage.__objects.items()}
-            json.dump(new_dict, fname)
+        """serializes __objects to the JSON file (path: __file_path)"""
+        new_dict = []
+        for obj in type(self).__objects.values():
+            new_dict.append(obj.to_dict())
+            # for key, obj in type(self).__objects.items():
+            #    new_dict[key] = obj.to_dict()
+        with open(type(self).__file_path, "w", encoding='utf-8') as file:
+            json.dump(new_dict, file)
+            # OR
+            # with open(type(self).__file_path, "w", encoding="utf-8") as file:
+            #   json.dump([obj.to_dict() for obj in self.all().values()], file)
 
     def reload(self):
-    """ reloads the JSON file and updates the objects in the dictionary """
-        if (os.path.isfile(FileStorage.__file_path)):
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as fname:
-                data = json.load(fname)
-                for key, val in data.items():
-                    FileStorage.__objects[key] = eval(val['__class__'])(**val)
+        """Deserializes the JSON file to __objects if it exists"""
+        if os.path.exists(type(self).__file_path) is True:
+            return
+            try:
+                with open(type(self).__file_path, "r") as file:
+                    new_obj = json.load(file)
+                    for key, val in new_obj.items():
+                        obj = self.class_dict[val['__class__']](**val)
+                        type(self).__objects[key] = obj
+            except Exception:
+                pass
